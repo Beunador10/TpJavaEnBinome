82b7a86e1035a44436f4bd9c309d1f5d777e8297
LaurentS10 <Laurents.stagech@celeriteholding.com>
2024-01-02 21:06:45 +0100

    first commit

---
+++ src/main/java/tpjava/ifri/bj/TpJavaEnBinome/TpJavaEnBinomeApplication.java
@@ -0,0 +1,13 @@
+package tpjava.ifri.bj.TpJavaEnBinome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJavaEnBinomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpJavaEnBinomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tpjava/ifri/bj/TpJavaEnBinome/TpJavaEnBinomeApplicationTests.java
@@ -0,0 +1,13 @@
+package tpjava.ifri.bj.TpJavaEnBinome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJavaEnBinomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
